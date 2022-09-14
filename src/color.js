9414da780b91d04d0634d38445d85454feffabaf
Evan Swanson <evan.sundelius@gmail.com>
2022-09-14 18:57:28 -0500

    Create color class

---
+++ src/color.js
@@ -0,0 +1,26 @@
+var characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+class Color {
+  constructor(code, locked) {
+    this.code = code;
+    this.locked = locked || false;
+  };
+};
+//~~~~~~~~~~~~~~~~~~~~
+function generateCode() {
+  var hexArray = []
+
+for (var i = 0; i < 6; i ++) {
+  hexArray.push(characters[makeRandomNumber(characters.length)]);
+  var withHash = `#${hexArray.join('')}`;
+  };
+return withHash;
+};
+//~~~~~~~~~~~~~~~~~~~~
+function makeRandomNumber(arrayLength) {
+  return Math.floor(Math.random() * arrayLength);
+};
+//~~~~~~~~~~~~~~~~~~~~
+var randomCode = generateCode();
+
+//var displayedColor1 = new Color(randomCode);
