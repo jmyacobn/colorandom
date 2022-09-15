4a8b124e80945516102a0633d522ba16b88c9f88
Evan Swanson <evan.sundelius@gmail.com>
2022-09-14 20:21:14 -0500

    Create visual display of random colors

--- src/palette.js
+++ src/palette.js
@@ -1,10 +1,10 @@
-class Palette {
-  constructor(color1, color2, color3, color4, color5) {
-    this.color1 = color1.code;
-    this.color2 = color2.code;
-    this.color3 = color3.code;
-    this.color4 = color4.code;
-    this.color5 = color5.code;
-    this.id = Date.now();
-  }
-}
+// class Palette {
+//   constructor(color1, color2, color3, color4, color5) {
+//     this.color1 = color1.code;
+//     this.color2 = color2.code;
+//     this.color3 = color3.code;
+//     this.color4 = color4.code;
+//     this.color5 = color5.code;
+//     this.id = Date.now();
+//   }
+// }
