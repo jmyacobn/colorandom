728680c5f0700a1b689b95d99c4f8e71a999832f
Jenn Yacoubian <jyacoubian@gmail.com>
2022-09-15 15:11:56 -0600

    Modify display palette function to access properties in array

--- src/scripts.js
+++ src/scripts.js
@@ -1,35 +1,21 @@
 //~~~~~~~~~~~~~~~~~~~~
-// class Color {
-//   constructor(code, locked) {
-//     this.code = code;
-//     this.locked = locked || false;
-//   };
-// };
-// class Palette {
-//   constructor(color1, color2, color3, color4, color5) {
-//     this.color1 = color1.code;
-//     this.color2 = color2.code;
-//     this.color3 = color3.code;
-//     this.color4 = color4.code;
-//     this.color5 = color5.code;
-//     this.id = Date.now();
-//   }
-// }
+var characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+var savedPalettes = [];
 //~~~~~~~~~~~~~~~~~~~~
-var characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-var text1 = document.querySelector('#p1')
-var text2 = document.querySelector('#p2')
-var text3 = document.querySelector('#p3')
-var text4 = document.querySelector('#p4')
-var text5 = document.querySelector('#p5')
-var locks = document.querySelectorAll('.lock')
-var lock1 = document.querySelector('#lock1')
-var lock2 = document.querySelector('#lock2')
-var lock3 = document.querySelector('#lock3')
-var lock4 = document.querySelector('#lock4')
-var lock5 = document.querySelector('#lock5')
-var newButton = document.querySelector('.new-palette-button')
-var fiveColorDisplay = document.querySelectorAll('.five-color-display')
+var text1 = document.querySelector('#p1');
+var text2 = document.querySelector('#p2');
+var text3 = document.querySelector('#p3');
+var text4 = document.querySelector('#p4');
+var text5 = document.querySelector('#p5');
+var locks = document.querySelectorAll('.lock');
+var lock1 = document.querySelector('#lock1');
+var lock2 = document.querySelector('#lock2');
+var lock3 = document.querySelector('#lock3');
+var lock4 = document.querySelector('#lock4');
+var lock5 = document.querySelector('#lock5');
+var newButton = document.querySelector('.new-palette-button');
+var fiveColorDisplay = document.querySelectorAll('.five-color-display');
+var saveButton = document.querySelector('.save-palette-button');
 //~~~~~~~~~~~~~~~~~~~~
 window.addEventListener('load', displayRandomPalette);
 newButton.addEventListener('click', displayRandomPalette);
@@ -38,8 +24,11 @@ lock2.addEventListener('click', toggleLock);
 lock3.addEventListener('click', toggleLock);
 lock4.addEventListener('click', toggleLock);
 lock5.addEventListener('click', toggleLock);
+saveButton.addEventListener('click', displaySavedPalette);
 //~~~~~~~~~~~~~~~~~~~~
-
+function displaySavedPalette() {
+  savedPalettes.push(fiveColorDisplay.childNode)
+}
 
 function displayRandomPalette() {
   if (lock1.innerText === '🔒') {
@@ -70,36 +59,31 @@ function displayRandomPalette() {
 
   var newPalette = new Palette(new1, new2, new3, new4, new5);
 
-  text1.innerText = newPalette.color1;
-  text2.innerText = newPalette.color2;
-  text3.innerText = newPalette.color3;
-  text4.innerText = newPalette.color4;
-  text5.innerText = newPalette.color5;
+  text1.innerText = newPalette.colors[0].code;
+  text2.innerText = newPalette.colors[1].code;
+  text3.innerText = newPalette.colors[2].code;
+  text4.innerText = newPalette.colors[3].code;
+  text5.innerText = newPalette.colors[4].code;
 
-  document.getElementById("sample-1").style.backgroundColor = newPalette.color1;
-  document.getElementById("sample-2").style.backgroundColor = newPalette.color2;
-  document.getElementById("sample-3").style.backgroundColor = newPalette.color3;
-  document.getElementById("sample-4").style.backgroundColor = newPalette.color4;
-  document.getElementById("sample-5").style.backgroundColor = newPalette.color5;
+  document.getElementById("sample-1").style.backgroundColor = newPalette.colors[0].code;
+  document.getElementById("sample-2").style.backgroundColor = newPalette.colors[1].code;
+  document.getElementById("sample-3").style.backgroundColor = newPalette.colors[2].code;
+  document.getElementById("sample-4").style.backgroundColor = newPalette.colors[3].code;
+  document.getElementById("sample-5").style.backgroundColor = newPalette.colors[4].code;
 };
 
 function toggleLock() {
-  //Change emoji AND force boolean toggle
   for (var i = 0; i < locks.length; i ++) {
     if (event.target === locks[i]) {
-      locks[i].innerText = '🔒'
-
+      var lockStatus = document.getElementById(`lock${i+1}`);
+      if (lockStatus.innerHTML === '🔓') {
+        lockStatus.innerHTML = '🔒';
+      } else {
+        lockStatus.innerHTML = '🔓';
+        }
+      }
     }
   }
-
-}
-
-// function makeRandomPalettes() {
-//
-// }
-//
-
-
 //~~~~~~~~~~~~~~~~~~~~
 function generateCode() {
   var hexArray = []
