b353c4b87b1d7f602ac79dd3acf3976f3727ab64
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-16 08:10:34 -0600

    Add functionality that gets each code of every color palette saved.

--- src/scripts.js
+++ src/scripts.js
@@ -1,67 +1,106 @@
-//Global Variables~~~~~~~~~~~~~~~~~~~~
-var characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-var savedPalettes = [];
-
-//Query Selectors~~~~~~~~~~~~~~~~~~~~
+// Global Variables~~~~~~~~~~~~~~~~~~~~
+var characters = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F'
+]
+var savedPalettes = []
+var newPalette
+// Query Selectors~~~~~~~~~~~~~~~~~~~~
 var texts = document.querySelectorAll('.text')
-var locks = document.querySelectorAll('.lock');
-var newButton = document.querySelector('.new-palette-button');
-var fiveColorDisplay = document.querySelectorAll('.five-color-display');
-var saveButton = document.querySelector('.save-palette-button');
+var locks = document.querySelectorAll('.lock')
+var newButton = document.querySelector('.new-palette-button')
+var fiveColorDisplay = document.querySelectorAll('.five-color-display')
+var saveButton = document.querySelector('.save-palette-button')
 
-//Event Listeners~~~~~~~~~~~~~~~~~~~~
-window.addEventListener('load', displayRandomPalette);
-newButton.addEventListener('click', displayRandomPalette);
+// Event Listeners~~~~~~~~~~~~~~~~~~~~
+window.addEventListener('load', displayRandomPalette)
+newButton.addEventListener('click', displayRandomPalette)
 for (var i = 0; i < locks.length; i++) {
   locks[i].addEventListener('click', toggleLock)
 }
-saveButton.addEventListener('click', displaySavedPalette);
+saveButton.addEventListener('click', displaySavedPalette)
 
-//Functions and Event Handlers~~~~~~~~~~~~~~~~~~~~
-function displaySavedPalette() {
+// Functions and Event Handlers~~~~~~~~~~~~~~~~~~~~
+function displaySavedPalette () {
+  savedPalettes.push(newPalette)
+  // the above line pushed the newPalette instances into the savedPalettes array.Console.log(savedPalettes) on this line to see what I am talking about.
+  // we need a for loop to iterate through the savedPalettes array, see below:
+  for (var i = 0; i < savedPalettes.length; i++) {
+    // now as we iterate throught the savedPalettes array, we need to acess 5 things from each newPalette instance that was saved in this array: we need the code of each of the 5 colors saved.
+    // as you can see for the new palette array, we need: (you can console log each line below to see!)
+    savedPalettes[i].colors[0].code // this gives us the first color of the 5 colors code for the ith element in the savedPalettes array (aka the ith newPalette that was stored there)
+    savedPalettes[i].colors[1].code // gives second color of the 5 codes
+    savedPalettes[i].colors[2].code //gives third color of the 5 codes
+    savedPalettes[i].colors[3].code //give fourth color of the 5 codes
+    savedPalettes[i].colors[4].code //gives fifth color of the 5 codes
+    //now we just have to take this 5 color codes of each newPalette and display them
+//I am thinking that we use the .innerHTML to create a new section with the unique id of the newPalette instance. In the new section we should have innerHTML that creates 5 sibling divs with the class of mini-palette so that we can size them all at once. Each of the 5 mini divs will also have .styling.backgroundColor
 
+  }
 }
 
-function displayRandomPalette() {
+function displayRandomPalette () {
   var newArray = []
-    for (var i = 0; i < 5; i ++) {
-      if (locks[i].innerText === '🔒') {
-        newArray[i] = new Color(texts[i].innerText, true);
-      } else {
-        newArray[i] = new Color(generateCode());
-      }
+  for (var i = 0; i < 5; i++) {
+    if (locks[i].innerText === '\uD83D\uDD12') {
+      newArray[i] = new Color(texts[i].innerText, true)
+    } else {
+      newArray[i] = new Color(generateCode())
     }
-
-  var newPalette = new Palette(newArray[0], newArray[1], newArray[2], newArray[3], newArray[4]);
-
-  for (var i = 0; i < newPalette.colors.length; i ++) {
-    document.getElementById(`sample-${i+1}`).style.backgroundColor = newPalette.colors[i].code;
-    texts[i].innerText = newPalette.colors[i].code;
   }
-};
 
-function toggleLock() {
-  for (var i = 0; i < locks.length; i ++) {
+  newPalette = new Palette(
+    newArray[0],
+    newArray[1],
+    newArray[2],
+    newArray[3],
+    newArray[4]
+  )
+
+  for (var i = 0; i < newPalette.colors.length; i++) {
+    document.getElementById(
+      `sample-${i + 1}`
+    ).style.backgroundColor = newPalette.colors[i].code
+    texts[i].innerText = newPalette.colors[i].code
+  }
+}
+
+function toggleLock () {
+  for (var i = 0; i < locks.length; i++) {
     if (event.target === locks[i]) {
-      if (locks[i].innerHTML === '🔓') {
-        locks[i].innerHTML = '🔒';
+      if (locks[i].innerHTML === '\uD83D\uDD13') {
+        locks[i].innerHTML = '\uD83D\uDD12'
       } else {
-        locks[i].innerHTML = '🔓';
-        }
+        locks[i].innerHTML = '\uD83D\uDD13'
       }
     }
   }
+}
 
-function generateCode() {
+function generateCode () {
   var hexArray = []
 
-for (var i = 0; i < 6; i ++) {
-  hexArray.push(characters[makeRandomNumber(characters.length)]);
-  var withHash = `#${hexArray.join('')}`;
-  };
-return withHash;
-};
+  for (var i = 0; i < 6; i++) {
+    hexArray.push(characters[makeRandomNumber(characters.length)])
+    var withHash = `#${hexArray.join('')}`
+  }
+  return withHash
+}
 
-function makeRandomNumber(arrayLength) {
-  return Math.floor(Math.random() * arrayLength);
-};
+function makeRandomNumber (arrayLength) {
+  return Math.floor(Math.random() * arrayLength)
+}
